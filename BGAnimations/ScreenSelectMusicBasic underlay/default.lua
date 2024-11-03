f2fdba01c14ef5af748e3119e86b81d8e1f324c0
Chris <christopher.reinbrecht@gmail.com>
2024-11-02 22:37:52 -0400

    Add missing timer functionality to new Lua select song screens

--- BGAnimations/ScreenSelectMusicBasic underlay/default.lua
+++ BGAnimations/ScreenSelectMusicBasic underlay/default.lua
@@ -39,7 +39,24 @@ local t = Def.ActorFrame {
     OnCommand=function(self)
         -- Change timing window to Easy
         LoadModule("Config.Save.lua")("SmartTimings",tostring("Pump Easy"),"Save/OutFoxPrefs.ini")
-    end
+    end,
+	-- Add timer functionality
+	InitCommand=function(self)
+		self:sleep(0.5):queuecommand("CheckTimer")
+	end,
+	CheckTimerCommand=function(self)
+		if SCREENMAN:GetTopScreen():GetChild("Timer"):GetSeconds() <= 0 then
+			self:queuecommand("TimerExpired")
+		else
+			self:sleep(0.5):queuecommand("CheckTimer")
+		end
+	end,
+	TimerExpiredCommand=function(self)
+		-- Set these or else we crash.
+        GAMESTATE:SetCurrentPlayMode("PlayMode_Regular")
+        GAMESTATE:SetCurrentStyle(GAMESTATE:GetNumSidesJoined() > 1 and "versus" or string.lower(ShortType(GAMESTATE:GetCurrentSteps(GAMESTATE:GetMasterPlayerNumber()))))
+        SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
+	end
 }
 
 -- The column thing
--- BGAnimations/ScreenSelectMusicFull underlay/default.lua
+++ BGAnimations/ScreenSelectMusicFull underlay/default.lua
@@ -1,5 +1,23 @@
 setenv("IsBasicMode", false)
-local t = Def.ActorFrame {}
+local t = Def.ActorFrame {
+	-- Add timer functionality
+	InitCommand=function(self)
+		self:sleep(0.5):queuecommand("CheckTimer")
+	end,
+	CheckTimerCommand=function(self)
+		if SCREENMAN:GetTopScreen():GetChild("Timer"):GetSeconds() <= 0 then
+			self:queuecommand("TimerExpired")
+		else
+			self:sleep(0.5):queuecommand("CheckTimer")
+		end
+	end,
+	TimerExpiredCommand=function(self)
+		-- Set these or else we crash.
+        GAMESTATE:SetCurrentPlayMode("PlayMode_Regular")
+        GAMESTATE:SetCurrentStyle(GAMESTATE:GetNumSidesJoined() > 1 and "versus" or string.lower(ShortType(GAMESTATE:GetCurrentSteps(GAMESTATE:GetMasterPlayerNumber()))))
+        SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
+	end
+}
 
 -- The column thing
 t[#t+1] = Def.Quad {
